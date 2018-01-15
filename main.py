38287ac945bf637a75989731e9e269bd8c35ca76
lamak <max.lamak@gmail.com>
2018-01-16 02:57:46 +1000

    added: - logging - ping check - input regular extension check

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from settings import *
 import xml.etree.ElementTree as ET
 import requests
 import os
+import logging
+import re
 
-#updater = Updater(token='your_token_here')
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+updater = Updater(token=telegram_token)
 dispatcher = updater.dispatcher
-
-xml_query = 'query.xml'
 
 
 def fsrar_get(server_name: str):
     fsrar, comment = '', ''
-    url = 'http://' + server_name + ':8080/diagnosis'
+    url = 'http://' + server_name + domain + ':8080/diagnosis'
     try:
-        for x in ET.fromstring(requests.get(url).text).findall('CN'):
+        for x in ET.fromstring(requests.get(url, timeout=1).text).findall('CN'):
             fsrar = x.text
     except requests.ConnectionError:
-        if os.system('ping %s -n 4 > NUL' % (server_name,)):
+        server_domain_name = server_name + domain
+        # if os.system('ping %s -n 4 > NUL' % (server_name,)):
+        if os.system('ping %s -c 2 > NUL' % (server_domain_name,)):
             comment = 'Пинга нет'
         else:
-            comment = 'Пинг есть'
+            comment = 'Пинг есть, УТМ недоступен'
+        # comment = 'УТМ недоступен'
     return fsrar, comment
 
 
@@ -39,14 +46,14 @@ def xml_make(fsrar: str):
 
 def xml_send(server_name: str):
     status, comment = '', ''
-    url = 'http://' + server_name + ':8080/opt/in/QueryClients_v2'
+    url = 'http://' + server_name + domain + ':8080/opt/in/QueryClients_v2'
     try:
         files = {'xml_file': (xml_query, open(xml_query, 'rb'), 'application/xml')}
         r = requests.post(url, files=files)
         if ET.fromstring(r.text).iter('sign'):
             status = 'OK'
         for error in ET.fromstring(r.text).iter('error'):
-            status = 'Ошибка подписи, проверьте ключ' + error.text
+            status = error.text + ', проверьте токен'
     except:
         status = 'Не удалось отправить XML'
     return status
@@ -59,10 +66,16 @@ def startCommand(bot, update):
 
 def textMessage(bot, update):
     utm_server = update.message.text
-    fsrarid, step1 = fsrar_get(utm_server)
-    step2 = xml_make(fsrarid)
-    status = xml_send(utm_server)
-    response = update.message.text + ' ' + fsrarid + ' ' + step1 + ' ' + step2 + ' ' + status
+    pattern = re.compile(re_pattern)
+    if pattern.match(utm_server):
+        step1, step2,status = '','',''
+        fsrarid, step1 = fsrar_get(utm_server)
+        if len(fsrarid) > 1:
+            step2 = xml_make(fsrarid)
+            status = xml_send(utm_server)
+            response = update.message.text + ' ' + fsrarid + ' ' + step1 + ' ' + step2 + ' ' + status
+    else:
+        response = 'Попробуйте короткое DNS имя, например vl44-srv03'
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
 
