2cdfb6ee288edced0001eb12ac53b23aedacbda4
Maxim Kapranov <max.lamak@gmail.com>
2019-11-14 19:12:59 +1000

    separated config file

--- bot.py
+++ bot.py
@@ -3,58 +3,37 @@ import os
 import logging
 import xml.etree.ElementTree as ET
 import requests
+import config
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import socks
 
-domain = '.severotorg.local'
-telegram_token = '489249497:AAGrMKgiadMb5-pUxpbBxH-shEFK8ebV9es'
-proxy = {
-    'proxy_url': 'socks5://p.remi.ru:1080',
-    # Optional, if you need authentication:
-    'urllib3_proxy_kwargs': {
-        'username': 'puser',
-        'password': 'ZikC@}nPQrDWnwao',
-    }
-}
-proxy = {'proxy_url': 'socks5://139.180.211.29:1080',}
-re_pattern = "\w+[-]\w+*?"cat 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-updater = Updater(token=telegram_token, request_kwargs=proxy)
-# updater = Updater(token=telegram_token)
-
+updater = Updater(token=config.telegram_token, request_kwargs=config.proxy)
 dispatcher = updater.dispatcher
 xml_query = 'query.xml'
 xml_wb = 'wb.xml'
 
 
 def fast_get(server_name: str):
-    server_domain_name = server_name + domain
+    server_domain_name = server_name + config.domain
     url = 'http://{0}:8080/diagnosis'.format(server_domain_name)
     # url_version = 'http://{0}:8080/info/version'.format(server_domain_name)
     try:
         # version = requests.get(url_version, timeout=2).text
         cn = ET.fromstring(requests.get(url).text).find('CN').text
-        fsrar = server_name + cn +' OK'
+        fsrar = 'OK [' + cn + '] ' + server_name
     except requests.ConnectionError:
-        fsrar = server_name + ' error'
+        fsrar = server_name + ' **error**'
     except requests.ReadTimeout:
-        fsrar = server_name + ' timeout'
+        fsrar = server_name + ' **timeout**'
     except ET.ParseError:
-        fsrar = server_name + ' no token'
-    except AttributeError:
-        fsrar = server_name + ' UTM broken'
-
+        fsrar = server_name + ' **no token**'
     return fsrar
 
 
 def fsrar_get(server_name: str):
     fsrar, error = '', ''
-    server_domain_name = server_name + domain
+    server_domain_name = server_name + config.domain
     url = 'http://{0}:8080/diagnosis'.format(server_domain_name)
     try:
         fsrar = ET.fromstring(requests.get(url).text).find('CN').text
@@ -84,7 +63,7 @@ def xml_make(fsrar: str):
 
 def xml_send(server_name: str):
     status = ''
-    server_domain_name = server_name + domain
+    server_domain_name = server_name + config.domain
     url = 'http://{0}:8080/opt/in/QueryClients_v2'.format(server_domain_name)
     try:
         files = {'xml_file': (xml_query, open(
@@ -93,26 +72,26 @@ def xml_send(server_name: str):
         if ET.fromstring(r.text).iter('sign'):
             status = 'OK'
         for error in ET.fromstring(r.text).iter('error'):
-            status = error.text + ', проверьте токен'
+            status = error.text + ', **проверьте токен**'
     except:
-        status = 'Не удалось отправить XML'
+        status = '**Не удалось отправить XML**'
     return status
 
 
 def filterCommand(bot, update):
-    with open("utms") as f: 
+    with open("utms") as f:
         data = f.read().splitlines()
     raw_data = []
     for server in data:
         status, error = '', ''
-        server_domain_name = server + domain
+        server_domain_name = server + config.domain
         url = 'http://{0}:8080/xhr/filter/reset'.format(server_domain_name)
         try:
-            result = requests.get(url).text
-        except Exception as e:
-            result = str(e)
+            result = requests.get(url, timeout=20).text
+        except requests.exceptions.ReadTimeout:
+            result = 'ConnectionError'
         raw_data.append(server + " " + result)
-    results = '\n'.join(raw_data)
+    results = '\n'.join([i for i in raw_data])
     bot.send_message(chat_id=update.message.chat_id,
                      text=results)
 
@@ -124,7 +103,7 @@ def startCommand(bot, update):
 
 def helpCommand(bot, update):
     help_text = ''
-    with open('help.txt', 'r', encoding='utf-8') as file:
+    with open('help.md', 'r', encoding='utf-8') as file:
         help_text = file.read()
     bot.send_message(chat_id=update.message.chat_id,
                      text=help_text, parse_mode='Markdown')
@@ -132,89 +111,33 @@ def helpCommand(bot, update):
 
 def faqCommand(bot, update):
     faq_text = ''
-    with open('faq.txt', 'r', encoding='utf-8') as file:
+    with open('faq.md', 'r', encoding='utf-8') as file:
         faq_text = file.read()
     bot.send_message(chat_id=update.message.chat_id,
                      text=faq_text, parse_mode='Markdown')
 
 
 def statusCommand(bot, update):
-    with open("utms") as f: 
+    with open("utms") as f:
         data = f.read().splitlines()
     raw_data = []
     for server in data:
         raw_data.append(fast_get(server))
-    results = '\n'.join([i for i in raw_data])
+    results = '\n'.join(raw_data)
     bot.send_message(chat_id=update.message.chat_id,
                      text=results)
-
-# class Result:
-#     """ Результаты опроса УТМ 
-#     С главной страницы получаем:
-#     * Состояние УТМ и лицензии
-#     * Сроки ключей ГОСТ, PKI
-#     * Состояние чеков
-#     * Организация из сертификата ГОСТ
-
-#     Фиксируются все ошибки при парсинге
-#     Данные УТМ переносятся в результат для вывода в шаблон Jinja2
-
-#     """
-
-#     def __init__(self, utm: Utm):
-#         self.utm: Utm = utm  # fsrar, server, title
-#         self.legal: str = ''
-#         self.gost: str = ''
-#         self.pki: str = ''
-#         self.cheques: str = ''
-#         self.status: bool = False
-#         self.licence: bool = False
-#         self.error: list = []
-#         self.fsrar: str = self.utm.fsrar
-#         self.host: str = self.utm.host
-#         self.url: str = self.utm.url()
-#         self.title: str = self.utm.title
-#         self.filter: bool = False
-
-# def get_docs_count(server_name: str):
-#     docs_in, docs_out, settings, licence, error = '', '', '', '', ''
-#     server_domain_name = server_name + domain
-#     url_index = 'http://{0}:8080'.format(server_domain_name)
-#     url_in = url_index + '/opt/out/waybill_v3'
-#     url_out = url_index + '/opt/in'
-
-#     try:
-#         docs_out = len(ET.fromstring(requests.get(url_out, timeout=2).text).findall('url'))
-#         docs_in = len(ET.fromstring(requests.get(url_in, timeout=2).text).findall('url'))
-        
-
-#     except (requests.ConnectionError, requests.ReadTimeout):
-#         if os.system('ping %s -n 2 > NUL' % (server_name,)):
-#             error = 'Связи нет'
-#         else:
-#             error = 'Связь есть, УТМ недоступен'
-#     except ET.ParseError:
-#         error = 'Проблема с УТМ, проверьте Рутокен'
-#     return counter, error
-
 
 
 def textMessage(bot, update):
     utm_server = update.message.text
-    pattern = re.compile(re_pattern)
+    pattern = re.compile(config.host_name_pattern)
     if pattern.match(utm_server):
-
         fsrarid, step1, step2, step3 = '', '', '', ''
         fsrarid, step1 = fsrar_get(utm_server)
         if fsrarid:
             step2 = xml_make(fsrarid)
             step3 = xml_send(utm_server)
-
-        # docs_in, docs_out = countin_get(utm_server)
-        # response = ' '.join([utm_server, step1, step2, step3, 'OUT: ', docs_out, 'IN: ', docs_in])
         response = ' '.join([utm_server, fsrarid, step1, step2, step3])
-
-
     else:
         response = 'Попробуйте короткое DNS имя, например vl44-srv03'
     bot.send_message(chat_id=update.message.chat_id, text=response)
@@ -227,14 +150,12 @@ faq_command_handler = CommandHandler('faq', faqCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 filter_command_handler = CommandHandler('filter', filterCommand)
 
-
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
 dispatcher.add_handler(help_command_handler)
 dispatcher.add_handler(faq_command_handler)
 dispatcher.add_handler(status_command_handler)
 dispatcher.add_handler(filter_command_handler)
-
 
 updater.start_polling(clean=True)
 
